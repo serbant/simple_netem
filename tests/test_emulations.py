b18f0224fe7fa0c70722b96028a1e7f0c5b10608
serbant <serbant@gmail.com>
2017-03-12 05:53:52 -0700

    add unit test for sad path in emulation.Emulation()
    see tests.test_emulations.TestEmulation.test_validate_and_args_raises()
    
    fix https://github.com/serbant/simple_netem/issues/1

--- tests/test_emulations.py
+++ tests/test_emulations.py
@@ -1,3 +1,4 @@
+""" unit tests for simple_netem.emulations """
 import os
 import sys
 
@@ -6,7 +7,7 @@ import pytest
 # modules under test are one directory up, make sure python can find them
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import emulations
+import emulations   # pylint:disable=F0401,C0413
 
 
 class TestEmulation(object):
@@ -26,10 +27,25 @@ class TestEmulation(object):
         test the validate_and_arg() method on happy paths
 
         '''
-        emulating = emulations._Emulation()
+        emulating = emulations.Emulation()
         emulating.emulation = emulation
         if defaults:
             emulating.validate_and_add(test_input, **defaults)
         else:
             emulating.validate_and_add(test_input)
         assert emulating.emulation == expected
+
+    @pytest.mark.parametrize(
+        'test_id,test_input,expect_in_error',
+        [('raise_no_number', 'aa aaa', 'a measurement')])
+    def test_validate_and_args_raises(
+            self, test_id, test_input, expect_in_error):
+        '''
+        test that validate_and_add() method raises the proper exceptions
+        '''
+        emulating = emulations.Emulation()
+        emulating.emulation = 'bogus'
+        with pytest.raises(emulations.EmulationArgTypeError) as error:
+            emulating.validate_and_add(test_input)
+
+        assert expect_in_error in error.value.args[0]
