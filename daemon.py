37369a1688965de6584d477d777d2ee303b06ae9
serbant <serbant@gmail.com>
2017-05-04 17:57:35 -0700

    module docstring added

--- daemon.py
+++ daemon.py
@@ -1,3 +1,32 @@
+'''
+.. _simple_netem_daemon:
+
+daemonizer module for the simple_netem package
+
+:module:     daemon
+
+:copyright:
+
+    Copyright 2017 Serban Teodorescu
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+:contact:    serbant@gmail.com
+
+exposes the functionality provided by the simple_netem package for remote
+access. this script can be executed as a system service on a Linux host
+
+'''
 from __future__ import unicode_literals, absolute_import, division
 
 import sys
