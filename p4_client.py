c33b478d5a741e763a2d49d18e22d1f6aced41a8
serbant <serbant@gmail.com>
2017-06-14 19:28:02 -0700

    Pyro4 client
    first cut, very basic implementation: creates a Pyro4.Proxy object to
    talk to the server. not tested beyond localhost yet

---
+++ p4_client.py
@@ -0,0 +1,48 @@
+'''
+.. _simple_netem_p4_client:
+
+pyro4 client module for the simple_netem control instances
+
+:module:     daemon
+
+:copyright:
+
+    Copyright 2017 Serban Teodorescu
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+:contact:    serbant@gmail.com
+
+provides classes for connecting to simple_netem control instances using the
+Pyro4 protocol
+
+'''
+from __future__ import unicode_literals, absolute_import, print_function
+
+import Pyro4
+
+import config
+import emulations
+
+__version__ = '0.0.1'
+
+
+class P4NetemClient(object):
+
+    def __init__(self, p4_uri=None):
+        Pyro4.config.SERIALIZER = 'pickle'
+        with Pyro4.Proxy(p4_uri) as p4_client:
+            p4_client._pyroHmacKey = config.P4_HMAC
+
+        self.client = p4_client
+        print(self.client.info)
